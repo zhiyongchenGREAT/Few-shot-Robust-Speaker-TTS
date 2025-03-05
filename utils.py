2a283f12db0ba5f5fee50bb08754d9863a6fa7f9
Shuhang Wu <shuhangwu@shu.edu.cn>
2025-03-05 22:38:08 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -80,35 +80,6 @@ def save_networks(networks, result_dir, name='', loss='', criterion=None):
         weights = criterion.state_dict()
         filename = '{}/checkpoints/{}_{}_criterion.pth'.format(result_dir, name, loss)
         torch.save(weights, filename)
-'''
-
-def save_networks(networks, result_dir, options, criterion=None):
-    model_name = f"{options['model']}_{options['loss']}_{options['item']}_{options['cs_fish']}_{options['cs_gpt']}_{options['cs_fishgpt']}_{options['cs_my']}"
-    checkpoints_dir = osp.join(result_dir, 'checkpoints')
-    mkdir_if_missing(checkpoints_dir)
-    
-    weights = networks.state_dict()
-    model_filename = osp.join(checkpoints_dir, f"{model_name}.pth")
-    torch.save(weights, model_filename)
-
-    if criterion:
-        criterion_weights = criterion.state_dict()
-        criterion_filename = osp.join(checkpoints_dir, f"{model_name}_criterion.pth")
-        torch.save(criterion_weights, criterion_filename)
-
-    print(f"Model saved to: {model_filename}")
-    if criterion:
-        print(f"Criterion saved to: {criterion_filename}")
-
-'''
-def save_GAN(netG, netD, result_dir, name=''):
-    mkdir_if_missing(osp.join(result_dir, 'checkpoints'))
-    weights = netG.state_dict()
-    filename = '{}/{}_G.pth'.format(result_dir, name)
-    torch.save(weights, filename)
-    weights = netD.state_dict()
-    filename = '{}/{}_D.pth'.format(result_dir, name)
-    torch.save(weights, filename)
 
 def load_networks(networks, result_dir, name='', loss='', criterion=None):
     weights = networks.state_dict()
@@ -120,18 +91,3 @@ def load_networks(networks, result_dir, name='', loss='', criterion=None):
         criterion.load_state_dict(torch.load(filename))
 
     return networks, criterion
-
-
-def save_networks_1(networks, result_dir, name='', loss='', criterion=None, embedding_model=None):
-    mkdir_if_missing(osp.join(result_dir, 'checkpoints'))
-    weights = networks.state_dict()
-    filename = '{}/checkpoints/{}_{}.pth'.format(result_dir, name, loss)
-    torch.save(weights, filename)
-    if criterion:
-        weights = criterion.state_dict()
-        filename = '{}/checkpoints/{}_{}_criterion.pth'.format(result_dir, name, loss)
-        torch.save(weights, filename)
-    if embedding_model:
-        weights = embedding_model.state_dict()
-        filename = '{}/checkpoints/{}_{}_eres2netv2.pth'.format(result_dir, name, loss)
-        torch.save(weights, filename)
